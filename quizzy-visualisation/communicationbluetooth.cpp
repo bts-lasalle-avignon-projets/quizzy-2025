9564f7936a3a8f52a4029d133203647024516adc
lraffin <louis.raffin.pro@gmail.com>
2025-03-17 10:57:08 +0100

    Ajout des attributs et méthodes

--- quizzy-visualisation/communicationbluetooth.cpp
+++ quizzy-visualisation/communicationbluetooth.cpp
@@ -11,6 +11,16 @@ CommunicationBluetooth::~CommunicationBluetooth()
     qDebug() << Q_FUNC_INFO << this;
 }
 
+void CommunicationBluetooth::demarrerServeur()
+{
+    // TODO
+}
+
+void CommunicationBluetooth::arreterServeur()
+{
+    // TODO
+}
+
 void CommunicationBluetooth::connecterAppareil()
 {
     // TODO
@@ -27,38 +37,5 @@ void CommunicationBluetooth::deconnecterAppareil()
 
 void CommunicationBluetooth::recevoirTrame()
 {
-    QByteArray donnees;
-
-    donnees            = socketDeLAppareil->readAll();
-    QString trameRecue = QString(donnees);
-
-    traiterTrame(trameRecue);
-}
-
-bool CommunicationBluetooth::traiterTrame(QString trameRecue)
-{
     // TODO
-    return true;
-}
-
-void CommunicationBluetooth::lireTrame(QString trameTraitee)
-{
-    // TODO
-    if(traiterTrame())
-    {
-        // TODO differencier l'envoie selon la trame recue
-        emit sessionParametree(trameTraitee[THEME],
-                               trameTraitee[TEMPS],
-                               trameTraitee[NOMBRE_DE_QUESTION),
-                               trameTraitee[NOM_JOUEUR_1],
-                               trameTraitee[NOM_JOUEUR_2]);
-        emit questionRecue(trameTraitee[QUESTION],
-                           trameTraitee[NUMERO_REPONSE],
-                           trameTraitee[PROPOSITION_A],
-                           trameTraitee[PROPOSITION_B],
-                           trameTraitee[PROPOSITION_C],
-                           trameTraitee[PROPOSITION_D]);
-        emit reponseJoueurRecue(trameTraitee[NUMERO_DU_JOUEUR],
-                                trameTraitee[NUMERO_REPONSE_JOUEUR]);
-    }
 }
--- quizzy-visualisation/communicationbluetooth.h
+++ quizzy-visualisation/communicationbluetooth.h
@@ -1,8 +1,9 @@
 #ifndef COMMUNICATIONBLUETOOTH_H
 #define COMMUNICATIONBLUETOOTH_H
 
+#include "quizzy.h"
+
 #include <QObject>
-#include <QMap>
 
 #include <QBluetoothServer>
 #include <QBluetoothSocket>
@@ -11,35 +12,6 @@
 #include <QBluetoothLocalDevice>
 #include <QBluetoothDeviceDiscoveryAgent>
 
-// Trame pour la configuration :
-//@@Thème;Tempsparquestion;Nombredequestion;NomJoueur1;NomJoueur2##
-
-// Trame pour chaque question :
-//@@Question;N°delaréponse;PropositionA;PropositionB;PropositionC;PropositionD##
-
-// Trame pour les réponses des joueurs
-//@@N°dujoueur;N°delapropositionchoisie##
-
-#define DEBUT_TRAME "@@"
-#define FIN_TRAME   "##"
-#define SEPARATEUR  ';'
-
-#define THEME              0;
-#define TEMPS              1;
-#define NOMBRE_DE_QUESTION 2;
-#define NOM_JOUEUR_1       3;
-#define NOM_JOUEUR_2       4;
-
-#define QUESTION       0;
-#define NUMERO_REPONSE 1;
-#define PROPOSITION_A  2;
-#define PROPOSITION_B  3;
-#define PROPOSITION_C  4;
-#define PROPOSITION_D  5;
-
-#define NUMERO_DU_JOUEUR      0;
-#define NUMERO_REPONSE_JOUEUR 1;
-
 class CommunicationBluetooth : public QObject
 {
     Q_OBJECT
@@ -47,6 +19,9 @@ class CommunicationBluetooth : public QObject
   public:
     CommunicationBluetooth(QObject* parent = nullptr);
     virtual ~CommunicationBluetooth();
+
+    void demarrerServeur();
+    void arreterServeur();
 
   private:
     bool etatDeConnexion;
@@ -59,9 +34,6 @@ class CommunicationBluetooth : public QObject
     QBluetoothSocket*     socketDeLAppareil;
     QBluetoothServiceInfo informationsDuService;
 
-    bool traiterTrame(QString trameRecue);
-    void lireTrame(QString trameTraitee);
-
   private slots:
     void connecterAppareil();
     void deconnecterAppareil();
@@ -70,15 +42,6 @@ class CommunicationBluetooth : public QObject
   signals:
     void appareilConnecte();
     void appareilDeconnecte();
-    void sessionParametree(QString theme,
-                           int     temps,
-                           int     nombreDeQuestion,
-                           QString nomJoueur1,
-                           QString nomJoueur2);
-    void questionRecue(QString     question,
-                       int         numeroDeLaReponse,
-                       QStringList propositions);
-    void reponseJoueurRecue(int numeroDuJoueur, int numeroPropositionChoisie);
 };
 
 #endif // COMMUNICATIONBLUETOOTH_H
