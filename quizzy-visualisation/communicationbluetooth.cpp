bcb1261c5459295a81c85c916190d446c576853f
lraffin <louis.raffin.pro@gmail.com>
2025-03-19 15:04:01 +0100

    Ajout des méthodes

--- quizzy-visualisation/communicationbluetooth.cpp
+++ quizzy-visualisation/communicationbluetooth.cpp
@@ -5,8 +5,94 @@ CommunicationBluetooth::CommunicationBluetooth(QObject* parent)
 {
     qDebug() << Q_FUNC_INFO << this << "parent" << parent;
 }
-
 CommunicationBluetooth::~CommunicationBluetooth()
 {
     qDebug() << Q_FUNC_INFO << this;
+}
+
+void CommunicationBluetooth::verifierLaConnexion()
+{
+    if(appareil.isValid())
+    {
+        activerBluetooth();
+        recupererInformationsAppareil();
+        rendreAppareilVisible();
+    }
+}
+
+void CommunicationBluetooth::activerBluetooth()
+{
+    appareil.powerOn();
+}
+
+void CommunicationBluetooth::recupererInformationsAppareil()
+{
+    nomDeLappareil      = appareil.name();
+    addresseDeLappareil = appareil.address();
+}
+
+void CommunicationBluetooth::rendreAppareilVisible()
+{
+    appareil.setHostMode(QBluetoothLocalDevice::HostDiscoverable);
+}
+
+void CommunicationBluetooth::demarrerServeur()
+{
+    if(serveur == nullptr)
+    {
+        serveur =
+          new QBluetoothServer(QBluetoothServiceInfo::RfcommProtocol, this);
+
+        connect(serveur,
+                SIGNAL(newConnection()),
+                this,
+                SLOT(connecterAppareil()));
+
+        QBluetoothUuid uuid(QBluetoothUuid::Rfcomm);
+        informationsDuService = serveur->listen(uuid, nomDuService);
+    }
+}
+
+void CommunicationBluetooth::arreterServeur()
+{
+    socketDeLAppareil->close();
+    delete socketDeLAppareil;
+    socketDeLAppareil = nullptr;
+
+    delete serveur;
+    serveur = nullptr;
+}
+
+void CommunicationBluetooth::connecterAppareil()
+{
+    socketDeLAppareil = serveur->nextPendingConnection();
+
+    connect(socketDeLAppareil,
+            SIGNAL(disconnected()),
+            this,
+            SLOT(deconnecterAppareil()));
+    connect(socketDeLAppareil,
+            SIGNAL(readyRead()),
+            this,
+            SLOT(recevoirTrame()));
+
+    etatDeConnexion = true;
+
+    emit appareilConnecte();
+}
+
+void CommunicationBluetooth::recevoirTrame()
+{
+    QByteArray donnees;
+
+    donnees       = socketDeLAppareil->readAll();
+    QString trame = QString(donnees);
+    qDebug() << Q_FUNC_INFO << "trame" << trame;
+}
+
+void CommunicationBluetooth::deconnecterAppareil()
+{
+    if(etatDeConnexion)
+        etatDeConnexion = false;
+    emit appareilDeconnecte();
 }
--- quizzy-visualisation/communicationbluetooth.h
+++ quizzy-visualisation/communicationbluetooth.h
@@ -1,15 +1,56 @@
 #ifndef COMMUNICATIONBLUETOOTH_H
 #define COMMUNICATIONBLUETOOTH_H
 
+#include "quizzy.h"
+
 #include <QObject>
+
+#include <QBluetoothServer>
+#include <QBluetoothSocket>
+#include <QBluetoothAddress>
+#include <QBluetoothDeviceInfo>
+#include <QBluetoothLocalDevice>
+#include <QBluetoothDeviceDiscoveryAgent>
+
+static const QString uuidDuService(
+  QStringLiteral("00001101-0000-1000-8000-00805F9B34FB"));
+static const QString nomDuService(QStringLiteral("Quizzy"));
 
 class CommunicationBluetooth : public QObject
 {
     Q_OBJECT
 
+  private:
+    bool etatDeConnexion;
+
+    QBluetoothLocalDevice appareil;
+    QBluetoothAddress     addresseDeLappareil;
+    QString               nomDeLappareil;
+
+    QBluetoothServer*     serveur;
+    QBluetoothSocket*     socketDeLAppareil;
+    QBluetoothServiceInfo informationsDuService;
+
   public:
     CommunicationBluetooth(QObject* parent = nullptr);
     virtual ~CommunicationBluetooth();
+
+    void demarrerServeur();
+    void arreterServeur();
+    void activerBluetooth();
+    void verifierLaConnexion();
+    void recupererInformationsAppareil();
+    void rendreAppareilVisible();
+
+  private slots:
+
+    void connecterAppareil();
+    void deconnecterAppareil();
+    void recevoirTrame();
+
+  signals:
+    void appareilConnecte();
+    void appareilDeconnecte();
 };
 
 #endif // COMMUNICATIONBLUETOOTH_H
