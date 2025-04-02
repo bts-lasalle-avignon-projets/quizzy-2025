673677e3d5cc086b6f934339f4e910e07aa6e2e0
lgasse <lenny.gasse@gmail.com>
2025-04-02 15:21:06 +0200

    ajout classe BDD

---
+++ quizzy-gestion/app/src/main/java/com/lasalle/quizzy/BaseDeDonnee.java
@@ -0,0 +1,8 @@
+package com.lasalle.quizzy;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+0
+public class BaseDeDonnee extends SQLiteOpenHelper {
+}
